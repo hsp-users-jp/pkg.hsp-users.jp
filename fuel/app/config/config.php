87e9fd39759da0590cc2e9ca01e0c6207f5052f8
sharkpp <webmaster@sharkpp.net>
2014-01-19 21:39:38 +0900

    バリデーションエラー時のメッセージを日本語化

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -78,7 +78,7 @@ return array(
 	/**
 	 * Localization & internationalization settings
 	 */
-	// 'language'           => 'en', // Default language
+	'language'           => 'ja', // Default language
 	// 'language_fallback'  => 'en', // Fallback language when file isn't available for default language
 	// 'locale'             => 'en_US', // PHP set_locale() setting, null to not set
 
@@ -273,6 +273,7 @@ return array(
 		'packages'  => array(
 			'orm',
 			'auth',
+			'language-pack-ja',
 		),
 
 		/**
