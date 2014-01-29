48bbe0249a199d204dab24ba0d6da6ff8c559135
sharkpp <webmaster@sharkpp.net>
2014-01-30 01:13:19 +0900

    仮登録状態でのエラー表示の文面を変更

--- fuel/app/views/auth/activation_warning.php
+++ fuel/app/views/auth/activation_warning.php
@@ -1,6 +1,7 @@
 <?php if (true): ?>
 <div class="alert alert-warning">
-  <span class="fa fa-exclamation-triangle"></span> アカウントの本登録が完了されていません。
-  登録から一ヶ月でパッケージなど含めアカウント情報が全て削除されてしまいます。
+  <p><span class="fa fa-exclamation-triangle"></span> アカウントの本登録が完了されていません。</p>
+  <p>登録から一ヶ月でパッケージなど含めアカウント情報が全て削除されてしまいます。</p>
+  <p>メールのアクティベーションリンクにアクセスし登録を完了してください。</p>
 </div>
 <?php endif ?>
